738b90870b6d35cdec3cdda019e80f241af6073a
Aroul S <arouls@icloud.com>
2026-03-15 13:43:50 +0530

    feedback and questions bug fix username

--- app/api/student-question/route.ts
+++ app/api/student-question/route.ts
@@ -1,5 +1,6 @@
 import Redis from "ioredis"
 import { NextResponse } from "next/server"
+import { cookies } from "next/headers"
 
 const redis = new Redis(process.env.REDIS_URL as string)
 
@@ -9,9 +10,29 @@ export async function POST(req:Request){
 
     const body = await req.json()
 
+    /* GET USER FROM COOKIE */
+
+    const session = cookies().get("student_session")?.value
+
+    let username = "unknown"
+
+    if(session){
+      const user = await redis.get(session)
+      if(user){
+        username = user
+      }
+    }
+
+    /* SAVE QUESTION */
+
     await redis.lpush(
       "studentQuestions",
-      JSON.stringify(body)
+      JSON.stringify({
+        student: username,
+        chapter: body.chapter,
+        questionNumber: body.questionNumber,
+        question: body.question
+      })
     )
 
     return NextResponse.json({success:true})
